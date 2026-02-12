9d01bfea8592682f073dbb6bc4ac463449ade401
Pratik Kale <pratikkale7661@gmail.com>
2026-02-12 12:58:32 +0530

    feat: prisma added

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
+import { prisma } from "./db/client";
+
 
 const app = express();
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -8,6 +11,18 @@ app.get("/", (req, res) => {
     })
 })
 
+app.post("/user", async (req, res) => {
+    const { username, password } = req.body
+
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password
+        }
+    })
+
+    res.status(200).json(user)
+})
 
 app.listen(8080, () => {
     console.log("api is running on the http://localhost:8080")
